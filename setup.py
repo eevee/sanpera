028046464b6b5b71f9b1518ebeb553421f7634d3
Eevee <git@veekun.com>
2012-05-13 17:14:08 -0700

    version bump
    I tagged 0.1.0 as something unbuildable, so, whoops.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def ext_module(module):
 
 setup(
     name='sanpera',
-    version='0.1.0',
+    version='0.1.1',
     description='Image manipulation library, powered by ImageMagick',
     author='Eevee',
     author_email='eevee.sanpera@veekun.com',
