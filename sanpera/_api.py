48f6a215deaf4dcc6d5aee676c8407ace170339d
weykent <weykent@weasyl.com>
2014-01-17 21:40:48 -0800

    Move imagemagick detection back to _api.py.
    Also, only invoke it if SANPERA_BUILD is set to 'y' or so, or if running from
    setup.py.

--- sanpera/_api.py
+++ sanpera/_api.py
@@ -1,8 +1,10 @@
 """ImageMagick API bindings, for cffi."""
 
 import atexit
-import json
 import os.path
+import shlex
+import subprocess
+from subprocess import CalledProcessError
 import sys
 
 import cffi
@@ -21,19 +23,77 @@ with open(os.path.join(here, '_api.h')) as f_headers:
     ffi.cdef(f_headers.read())
 with open(os.path.join(here, '_apiconfig.json')) as f_config:
 
-    config = json.load(f_config)
 
-if sys.version_info > (3,):
-    extension_kwargs = config
-else:
-    extension_kwargs = {}
-    for key, value in config.iteritems():
-        extension_kwargs[key.encode()] = [v.encode() for v in value]
+def check_output(*args, **kwargs):
+    """Rough implementation of `subprocess.check_output`, which doesn't exist
+    until 2.7.
+    """
+    proc = subprocess.Popen(stdout=subprocess.PIPE, *args, **kwargs)
+    out, _err = proc.communicate()
+    retcode = proc.poll()
+    if retcode != 0:
+        cmd = args[0]
+        raise CalledProcessError(retcode, cmd)
+    return out
+
+
+def find_imagemagick_configuration():
+    """Find out where ImageMagick is and how it was built.  Return a dict of
+    distutils extension-building arguments.
+    """
+
+    # Easiest way: the user is telling us what to use.
+    env_cflags = os.environ.get('SANPERA_IMAGEMAGICK_CFLAGS')
+    env_ldflags = os.environ.get('SANPERA_IMAGEMAGICK_LDFLAGS')
+    if env_cflags is not None or env_ldflags is not None:
+        return dict(
+            extra_compile_args=env_cflags or '',
+            extra_link_args=env_ldflags or '',
+        )
+
+    # Easy way: pkg-config, part of freedesktop
+    # Note that ImageMagick ships with its own similar program `Magick-config`,
+    # but it's just a tiny wrapper around `pkg-config`, so why it even exists
+    # is a bit of a mystery.
+    try:
+        compile_args = check_output(['pkg-config', 'ImageMagick', '--cflags'])
+        link_args = check_output(['pkg-config', 'ImageMagick', '--libs'])
+    except OSError:
+        pass
+    except CalledProcessError:
+        # This means that pkg-config exists, but ImageMagick isn't registered
+        # with it.  Odd, but not worth giving up yet.
+        pass
+    else:
+        return dict(
+            extra_compile_args=shlex.split(compile_args),
+            extra_link_args=shlex.split(link_args),
+        )
+
+    # TODO this could use more fallback, but IM builds itself with different
+    # names (as of some recent version, anyway) depending on quantum depth et
+    # al.  the `wand` project just brute-force searches for the one it wants.
+    # perhaps we could do something similar.  also, we only need the header
+    # files for the build, so it would be nice to get away with only hunting
+    # down the library for normal use.
+
+    raise RuntimeError(
+        "Can't find ImageMagick installation!\n"
+        "If you're pretty sure you have it installed, please either install\n"
+        "pkg-config or tell me how to find libraries on your platform."
+    )
+
+
+extension_kwargs = {}
+if os.environ.get('SANPERA_BUILD', '').lower().startswith('y'):
+    extension_kwargs = find_imagemagick_configuration()
+
 
 with open(os.path.join(here, '_api.c')) as f_stub:
     lib = ffi.verify(
         f_stub.read(),
         ext_package='sanpera',
+        modulename='_api_bridge',
         **extension_kwargs)
 
 # ImageMagick initialization
--- setup.py
+++ setup.py
@@ -1,83 +1,16 @@
-import io
-import json
 import os
-import shlex
-import subprocess
-from subprocess import CalledProcessError
 import sys
 
 from setuptools import setup
 from setuptools.dist import Distribution
 
+
 # Do this first, so setuptools installs cffi immediately before trying to do
 # the below import
 Distribution(dict(setup_requires='cffi'))
 
-
-def check_output(*args, **kwargs):
-    """Rough implementation of `subprocess.check_output`, which doesn't exist
-    until 2.7.
-    """
-    proc = subprocess.Popen(stdout=subprocess.PIPE, *args, **kwargs)
-    out, _err = proc.communicate()
-    retcode = proc.poll()
-    if retcode != 0:
-        cmd = args[0]
-        raise CalledProcessError(retcode, cmd)
-    return out
-
-
-def find_imagemagick_configuration():
-    """Find out where ImageMagick is and how it was built.  Return a dict of
-    distutils extension-building arguments.
-    """
-
-    # Easiest way: the user is telling us what to use.
-    env_cflags = os.environ.get('SANPERA_IMAGEMAGICK_CFLAGS')
-    env_ldflags = os.environ.get('SANPERA_IMAGEMAGICK_LDFLAGS')
-    if env_cflags is not None or env_ldflags is not None:
-        return dict(
-            extra_compile_args=env_cflags or '',
-            extra_link_args=env_ldflags or '',
-        )
-
-    # Easy way: pkg-config, part of freedesktop
-    # Note that ImageMagick ships with its own similar program `Magick-config`,
-    # but it's just a tiny wrapper around `pkg-config`, so why it even exists
-    # is a bit of a mystery.
-    try:
-        compile_args = check_output(['pkg-config', 'ImageMagick', '--cflags'])
-        link_args = check_output(['pkg-config', 'ImageMagick', '--libs'])
-    except OSError:
-        pass
-    except CalledProcessError:
-        # This means that pkg-config exists, but ImageMagick isn't registered
-        # with it.  Odd, but not worth giving up yet.
-        pass
-    else:
-        return dict(
-            extra_compile_args=shlex.split(compile_args),
-            extra_link_args=shlex.split(link_args),
-        )
-
-    # TODO this could use more fallback, but IM builds itself with different
-    # names (as of some recent version, anyway) depending on quantum depth et
-    # al.  the `wand` project just brute-force searches for the one it wants.
-    # perhaps we could do something similar.  also, we only need the header
-    # files for the build, so it would be nice to get away with only hunting
-    # down the library for normal use.
-
-    raise RuntimeError(
-        "Can't find ImageMagick installation!\n"
-        "If you're pretty sure you have it installed, please either install\n"
-        "pkg-config or tell me how to find libraries on your platform."
-    )
-
-
-config = find_imagemagick_configuration()
-with open('sanpera/_apiconfig.json', 'w') as f_config:
-    json.dump(config, f_config)
-
+# Set this so the imagemagick flags will get sniffed out.
+os.environ['SANPERA_BUILD'] = 'yes'
 
 from sanpera._api import ffi
 
@@ -105,7 +38,7 @@ setup(
 
     packages=['sanpera'],
     package_data={
-        'sanpera': ['_api.c', '_api.h', '_apiconfig.json'],
+        'sanpera': ['_api.c', '_api.h'],
     },
     install_requires=BACKPORTS + [
         'cffi',
